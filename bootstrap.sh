292c555c42e12808601a1f6a26505020981f4cf7
Marvin Guerra <mguerra@figma.com>
2025-05-19 15:10:47 -0700

    updates to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,6 +16,26 @@ brew install gradle
 brew install go
 brew install tree
 
+# Install Default Apps with brew-cask
+brew tap homebrew/cask-drivers
+
+brew install arc
+brew install caffeine
+brew install docker
+brew install iterm2
+brew install logitune
+brew install monitorcontrol
+brew install notion
+brew install postman
+brew install raycast
+brew install slack
+brew install sonos
+brew install sourcetree
+brew install spotify
+brew install sublime-text
+brew install ticktick
+brew install vlc
+
 # Install apps from App Store
 brew install mas
 while true; do
@@ -27,20 +47,7 @@ while true; do
     esac
 done
 
-# Install Default Apps with brew-cask
-brew tap homebrew/cask-drivers
-
-brew install caffeine
-brew install cron
-brew install iterm2
-brew install MonitorControl
-brew install postman
-brew install raycast
-brew install sonos
-brew install sourcetree
-brew install spectacle
-brew install spotify
-brew install sublime-text
+mas install 1476964367 # InYourFace Reminders
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
